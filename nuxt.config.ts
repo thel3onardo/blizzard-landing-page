b699ecb207413a3b2241666f7f3cd1c524f7ffd0
thel3onardo <leonardo.contato03@gmail.com>
2022-12-08 14:10:20 -0300

    feat: install @nuxtjs/googlefonts module; setup project's font families

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss'
-    ]
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/google-fonts'
+    ],
+
+    googleFonts: {
+        families: {
+            Poppins: true
+        }
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
 <template>
-    <h1 class="text-red-500">Olá</h1>
+    <h1 class="text-red-500 font-poppins">Olá</h1>
+    <h3 class="text-green-500">Meu nome é leonardo de Oliveira da Silva</h3>
 </template>
