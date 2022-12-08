3041324b989e94102059b17487765638c520a240
thel3onardo <leonardo.contato03@gmail.com>
2022-12-08 14:41:05 -0300

    feat: create button component

---
+++ components/Button.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    const props = defineProps({
+        background: {
+            type: Boolean,
+            required: false,
+            default: false,
+        }
+    })
+    onMounted(() => {
+        console.log(props)
+    })
+</script>
+
+<template>
+    <button 
+        :class="props.background ? 'bg-blue-500' : 'bg-transparent border border-white'"
+        class="text-white text-sm font-medium  rounded-[3px]"
+    >
+        <slot></slot>
+    </button>
+</template>
