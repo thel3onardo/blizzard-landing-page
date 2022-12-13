6e40a4ea28238264a7f3a3d09c497da07f1c5d21
thel3onardo <leonardo.contato03@gmail.com>
2022-12-12 21:26:56 -0300

    refactor: adjust image styles of GameCard component

--- components/GameCard.vue
+++ components/GameCard.vue
@@ -17,8 +17,8 @@
 
 <template>
   <div class="flex flex-col text-white">
-    <div class="w-full rounded-lg overflow-hidden">
-        <img :src="imagePath" class="w-full h-full">
+    <div class="w-full rounded-lg overflow-hidden lg:max-h-[400px]">
+        <img :src="imagePath" class="w-full h-full object-cover">
     </div>
     <h2 class="font-semibold text-lg mt-6">{{ title }}</h2>
     <p class="font-normal text-sm opacity-80 mt-1">{{ genre }}</p>
