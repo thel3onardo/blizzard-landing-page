5c9421b0b6308fd35a76f78aad921fd2acb10423
thel3onardo <leonardo.contato03@gmail.com>
2022-12-12 21:21:01 -0300

    refactor: make visually improvements to GameCard component

--- components/GameCard.vue
+++ components/GameCard.vue
@@ -17,10 +17,10 @@
 
 <template>
   <div class="flex flex-col text-white">
-    <div class="w-full">
+    <div class="w-full rounded-lg overflow-hidden">
         <img :src="imagePath" class="w-full h-full">
     </div>
-    <h2 class="font-semibold text-lg my-6">{{ title }}</h2>
+    <h2 class="font-semibold text-lg mt-6">{{ title }}</h2>
     <p class="font-normal text-sm opacity-80 mt-1">{{ genre }}</p>
   </div>
 </template>
